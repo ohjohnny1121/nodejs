78f60fdef9d5006b4f507ecb2d2adfcd107a6648
HsuOuYang <Hsu_OuYang@unimicron.com>
2024-12-27 17:12:39 +0800

    [revise/aoi.js,aoi.yaml] 修改History API

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { dailyAdd, stackAdd } = require('./daily/dailyFunc.js');
 // 獲取主機名稱
 const hostname = os.hostname();
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ['http://localhost:5173', 'http://localhost:3000']);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
