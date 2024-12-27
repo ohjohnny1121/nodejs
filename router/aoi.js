a0b4dc161469027af6b9955bb7d0feda2fd3dc8a
HsuOuYang <Hsu_OuYang@unimicron.com>
2024-12-27 10:53:07 +0800

    [revise/mysql.js] 修改mysql ，避免連結池錯誤

--- router/aoi.js
+++ router/aoi.js
@@ -67,16 +67,12 @@ router.post('/aoi-revise-remark/:lotnum/:remark', async (req, res) => {
 
 
 router.get('/aoidaily/:startDate/:endDate/:factory', async (req, res) => {
-
-    
     let connection;
-
-    
     try {
         const { startDate, endDate, factory } = req.params;
         const startTimestamp = Number(startDate);
-        const endTimestamp = Number(endDate);   
-        console.log(startTimestamp,convertTimestampToFormattedDate(startTimestamp), convertTimestampToFormattedDate(endTimestamp));
+        const endTimestamp = Number(endDate);
+        console.log(startTimestamp, convertTimestampToFormattedDate(startTimestamp), convertTimestampToFormattedDate(endTimestamp));
 
         if (!startDate || !endDate) {
             return res.status(400).json({
@@ -85,75 +81,52 @@ router.get('/aoidaily/:startDate/:endDate/:factory', async (req, res) => {
                 time: timestampToYMDHIS(new Date())
             });
         }
+
         // 獲取連接
         connection = await mysqlConnection(getDbConfig('aoi'));
-        
-        try {
-            await connection.beginTransaction();
-            
-            // 生成開始日期和結束日期
-            const sqlStr = `SELECT 
-                            factory,
-                            prod_class,
-                            part_no,
-                            lot_num,
-                            triger,
-                            bef_yield,
-                            yield,
-                            DATE_FORMAT(time, '%Y-%m-%d %H:%i:%s') as time,
-                            c_top_1,
-                            c_top1,
-                            c_top_2,
-                            c_top2,
-                            c_top_3,
-                            c_top3,
-                            s_top_1,
-                            s_top1,
-                            s_top_2,
-                            s_top2,
-                            s_top_3,
-                            s_top3,
-                            remark,
-                            mp_lt_x*mp_lt_y upp
-                            FROM aoi_yield_defect 
-                            WHERE time >= '${convertTimestampToFormattedDate(startTimestamp)}' 
-                            AND time <= '${convertTimestampToFormattedDate(endTimestamp)}' 
-                            AND factory = '${factory}'`;
-            // console.log(sqlStr);
-            const result = await queryFunc(connection, sqlStr);
-            
-            
-            // 插入新的權限
-            // for (const auth of authority) {
-            //     //可以抓出id這個資料表的資料數量
-            //     const sqlStrid = `SELECT COUNT(*) as Count FROM Whitelist`;
-            //     const resultid = await queryFunc(connection, sqlStrid);
-            //     const id = resultid[0].Count + 1;
-            //     const sqlStrinsert = `
-            //         INSERT INTO Whitelist (id,uid, authority, creator, isdelete,time) 
-            //         VALUES ('${id}','${uid}', '${auth}', '${creator}', 'false','${dateStr}')`;
-            //         console.log(sqlStrinsert);
-            //     await queryFunc(connection, sqlStrinsert);
-            // }
-            
-            await connection.commit();
-            
-            res.status(200).json({
-                status: 'success',
-                message: '成功',
-                data: result,
-                time: timestampToYMDHIS(new Date())
-            });
-            
-        } catch (error) {
-            if (connection) {
-                await connection.rollback();
-                console.log('回滾');
-                await connection.release();
-            }
-            throw error;
+
+        if (!connection) {
+            throw new Error('無法獲取數據庫連接');
         }
-        
+
+        // 使用參數化查詢
+        const sqlStr = `SELECT 
+                        factory,
+                        prod_class,
+                        part_no,
+                        lot_num,
+                        triger,
+                        bef_yield,
+                        yield,
+                        DATE_FORMAT(time, '%Y-%m-%d %H:%i:%s') as time,
+                        c_top_1,
+                        c_top1,
+                        c_top_2,
+                        c_top2,
+                        c_top_3,
+                        c_top3,
+                        s_top_1,
+                        s_top1,
+                        s_top_2,
+                        s_top2,
+                        s_top_3,
+                        s_top3,
+                        remark,
+                        mp_lt_x*mp_lt_y upp
+                        FROM aoi_yield_defect 
+                        WHERE time >= ? 
+                        AND time <= ? 
+                        AND factory = ?`;
+
+        const result = await queryFunc(connection, sqlStr, [convertTimestampToFormattedDate(startTimestamp), convertTimestampToFormattedDate(endTimestamp), factory]);
+
+        res.status(200).json({
+            status: 'success',
+            message: '成功',
+            data: result,
+            time: timestampToYMDHIS(new Date())
+        });
+
     } catch (error) {
         console.error('操作失敗:', error);
         res.status(500).json({
