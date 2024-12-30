039465354761cc8b748e9a8e04fa7c514fd32110
HsuOuYang <Hsu_OuYang@unimicron.com>
2024-12-30 14:17:18 +0800

    [revise/mysql.js] 啟動心跳查詢

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ const swaggerAoi = YAML.load(path.join(__dirname, 'api', 'aoi.yaml'));
 const swaggerUser = YAML.load(path.join(__dirname, 'api', 'user.yaml'));
 
 // 設定每天早上 8:00 執行
-cron.schedule('10 05 14 * * *', async () => {
-cron.schedule('10 25 09 * * *', async () => {
+cron.schedule('10 23 17 * * *', async () => {
     try {
         console.log(`${API_BASE_URL}/daily/aoi/sndailyadd 開始執行 SN AOI 每日資料更新`);
         await stackAdd(`${API_BASE_URL}/daily/aoi/sndailyadd`);
